418be32975e1a38321794bd1a51c2de14bc516d8
Flavio Patti <flaviogiovannipatti@gmail.com>
2025-10-17 14:53:30 +0200

    feat(cart): added toast on cart actions

--- src/components/atom/cartItem/CartItem.tsx
+++ src/components/atom/cartItem/CartItem.tsx
@@ -1,4 +1,5 @@
-import { useActionState } from "react";
+import { toast } from "sonner";
+import { useActionState, useEffect } from "react";
 import { useFormStatus } from "react-dom";
 import { Icon } from "../icon/Icon";
 import { CartItemProps } from "./cartItem.type";
@@ -12,32 +13,33 @@ const CartItem = ({
   quantity,
   classname,
 }: CartItemProps) => {
-  const [, formAction] = useActionState(removeCartItem, {
+  const [formState, formAction] = useActionState(removeCartItem, {
     success: false,
     message: "",
   });
+  useEffect(() => {
+    if (formState?.success === true) {
+      toast.success(formState.message);
+    } else if (formState?.message ? formState.success === false : false) {
+      toast.error(formState?.message);
+    }
+  }, [formState]);
 
-  const DeleteIconButton = () => {
-    const { pending } = useFormStatus();
-
-    return (
-      <button type="submit" className="contents" disabled={pending}>
-        <Icon
-          name={"Delete"}
-          size={"24"}
-          weight={"regular"}
-          variant={"danger"}
-        />
-      </button>
-    );
-  };
+  const { pending } = useFormStatus();
   return (
     <div
       className={`flex items-center justify-between gap-4 py-4 ${classname}`}
     >
       <form action={formAction} className="contents">
         <input type="hidden" name="productId" value={productId} />
-        <DeleteIconButton />
+        <button type="submit" className="contents" disabled={pending}>
+          <Icon
+            name={"Delete"}
+            size={"24"}
+            weight={"regular"}
+            variant={"danger"}
+          />
+        </button>
       </form>
 
       <div className="max-w-48 flex-1">

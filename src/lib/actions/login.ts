691cac86abb2b1dbf0262fbb8f6c7537253a0e1b
Flavio Patti <flaviogiovannipatti@gmail.com>
2025-10-20 12:46:24 +0200

    added vercel key

--- src/lib/actions/login.ts
+++ src/lib/actions/login.ts
@@ -56,7 +56,7 @@ export async function loginWithGoogle() {
   const { data, error } = await supabase.auth.signInWithOAuth({
     provider: "google",
     options: {
-      redirectTo: `${process.env.NEXT_PUBLIC_SITE_URL}/callback`,
+      redirectTo: `${process.env.NEXT_PUBLIC_VERCEL_URL}/callback`,
     },
   });
 
